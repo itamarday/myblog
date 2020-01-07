1652ece150f8dffd9a977a95ede9c2e796b3362d
itamarday <58741322+itamarday@users.noreply.github.com>
2020-01-07 20:01:34 +0200

    Add files via upload

--- java.js
+++ java.js
@@ -1,4 +1,9 @@
 function techfun() {
-    document.getElementById("tech").innerHTML = "סרגי!";
+    document.getElementById("tech").innerHTML = "סרגי! &#x1F44D;";
+    
 }
+function imgrev(){
+    document.getElementById("img2cange").style.visibility='visible';
+    }
+    
 
